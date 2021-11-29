e031d1427d20c41c07575072e988b0e33bf3b2f7
brend <brendanjelashe@gmail.com>
2021-11-29 15:50:52 +0200

    timetable of the first coding lesson

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("*********************timetable******************");
+        System.out.println("Accounting12:30");
+        System.out.println("english14:30");
+        System.out.println("Afrikaans16:30");
+        System.out.println("Math.lit 18:30");
+    }
+}
